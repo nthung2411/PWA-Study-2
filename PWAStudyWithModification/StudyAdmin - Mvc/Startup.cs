2c2e27e46804cdf1adcdde478ded1bea0a2b0fe9
nthung2411 <nthung2411@gmail.com>
2016-12-28 09:45:29 +0700

    Study Admin - able to setup content
    add Study Admin - MVC

---
+++ PWAStudyWithModification/StudyAdmin - Mvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudyAdmin___Mvc.Startup))]
+namespace StudyAdmin___Mvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
